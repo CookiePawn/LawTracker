59cf4d4ec3263d862391323c824070eb609f9965
CookiePawn <cookiepawn@naver.com>
2025-05-07 23:37:58 +0900

    [fix][home][fix home]

--- src/screens/Tab/Home/Home.tsx
+++ src/screens/Tab/Home/Home.tsx
@@ -82,6 +82,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'space-between',
+    marginBottom: 20,
   },
   headerTitle: {
     fontSize: 24,
